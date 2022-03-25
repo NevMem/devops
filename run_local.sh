31952eafcef1d85bc1ae6b2bfff7f82685553c24
NevMem <memlolkek@gmail.com>
2022-03-25 19:13:29 +0300

    hmm

--- run_local.sh
+++ run_local.sh
@@ -1,16 +1,18 @@
-mkdir tmp
-cd tmp
-git clone https://github.com/google/googletest.git
-cd ../
-mkdir task-ci/3rdparty
-cp -r tmp/googletest task-ci/3rdparty/
-rm -rf tmp
-ls -la task-ci/3rdparty/
+# mkdir tmp
+# cd tmp
+# git clone https://github.com/google/googletest.git
+# cd ../
+# mkdir task-ci/3rdparty
+# cp -r tmp/googletest task-ci/3rdparty/
+# rm -rf tmp
+# ls -la task-ci/3rdparty/
 
-cd task-ci && cmake -B ${{github.workspace}}/task-ci/build -DCMAKE_BUILD_TYPE=${{env.BUILD_TYPE}}
+mkdir task-ci/build
+
+cd task-ci && GOOGLE_TEST_DIR=googletest cmake -B ./build -DCMAKE_BUILD_TYPE=Test
 cd ../
 
-cd task-ci && cmake --build ${{github.workspace}}/task-ci/build --config ${{env.BUILD_TYPE}}
+cd task-ci && cmake --build ./build --config Test
 cd ../
 
-# cd task-ci/build && ctest -C ${{env.BUILD_TYPE}}
+cd task-ci/bin && ls -la && ctest -C Test
